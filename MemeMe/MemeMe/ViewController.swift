f85e1e7d1e745d9460165d122353380bcd4868ed
Timi Tejumola <tejumoladavid@gmail.com>
2020-03-18 21:39:18 +0100

    first commit for mememe app

---
+++ MemeMe/MemeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Timi Tejumola on 18/03/2020.
+//  Copyright © 2020 Timi Tejumola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
